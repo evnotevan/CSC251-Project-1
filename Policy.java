930d97d0cd66d6df81cc09ba29e0fef69c8e71ac
evnotevan <alexanderevan775@gmail.com>
2024-02-19 14:47:03 -0500

    Moved fields and methods to the PolicyHolder

--- Policy.java
+++ Policy.java
@@ -1,108 +1,45 @@
-import java.util.Scanner;
-
-
-/**
- * The Policy class represents an insurance policy with various attributes.
- */
 public class Policy {
     private String policyNumber;
     private String providerName;
-    private String firstName;
-    private String lastName;
-    private int age;
-    private String smokingStatus;
-    private int height;
-    private int weight;
+    private double coverageAmount;
+    private double premiumAmount;
 
-    /**
-     * Default constructor for Policy class.
-     * Initializes all attributes to default values.
-     */
-    public Policy() {
-        this.policyNumber = "";
-        this.providerName = "";
-        this.firstName = "";
-        this.lastName = "";
-        this.age = 0;
-        this.smokingStatus = "non-smoker";
-        this.height = 0;
-        this.weight = 0;
-    }
-
-    /**
-     * Parameterized constructor for Policy class.
-     *
-     * @param policyNumber   The policy number.
-     * @param providerName   The name of the insurance provider.
-     * @param firstName      The first name of the policy holder.
-     * @param lastName       The last name of the policy holder.
-     * @param age            The age of the policy holder.
-     * @param smokingStatus  The smoking status of the policy holder.
-     * @param height         The height of the policy holder.
-     * @param weight         The weight of the policy holder.
-     */
-    public Policy(String policyNumber, String providerName, String firstName, String lastName,
-                  int age, String smokingStatus, int height, int weight) {
+    public Policy(String policyNumber, String providerName, double coverageAmount, double premiumAmount) {
         this.policyNumber = policyNumber;
         this.providerName = providerName;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.smokingStatus = smokingStatus;
-        this.height = height;
-        this.weight = weight;
+        this.coverageAmount = coverageAmount;
+        this.premiumAmount = premiumAmount;
     }
 
-    /**
-     * Getter for policyNumber attribute.
-     *
-     * @return The policy number.
-     */
     public String getPolicyNumber() {
         return policyNumber;
     }
 
-    /**
-     * Setter for policyNumber attribute.
-     *
-     * @param policyNumber The new policy number to set.
-     */
     public void setPolicyNumber(String policyNumber) {
         this.policyNumber = policyNumber;
     }
 
-    // Similar documentation comments for other getter and setter methods...
-
-    /**
-     * Calculates the Body Mass Index (BMI) based on weight and height.
-     *
-     * @return The calculated BMI.
-     */
-    public double calculateBMI() {
-        return (weight * 703.0) / (height * height);
+    public String getProviderName() {
+        return providerName;
     }
 
-    /**
-     * Calculates the insurance price based on BMI, age, and smoking status.
-     *
-     * @return The calculated insurance price.
-     */
-    public double calculateInsurancePrice() {
-        double additionalFee = (calculateBMI() - 35) * 20;
-        double baseFee = 600;
+    public void setProviderName(String providerName) {
+        this.providerName = providerName;
+    }
 
-        if (age > 50) {
-            baseFee += 75;
-        }
+    public double getCoverageAmount() {
+        return coverageAmount;
+    }
 
-        if (smokingStatus.equals("smoker")) {
-            baseFee += 100;
-        }
+    public void setCoverageAmount(double coverageAmount) {
+        this.coverageAmount = coverageAmount;
+    }
 
-        return baseFee + additionalFee;
+    public double getPremiumAmount() {
+        return premiumAmount;
+    }
+
+    public void setPremiumAmount(double premiumAmount) {
+        this.premiumAmount = premiumAmount;
     }
 }
-
-
-
-
